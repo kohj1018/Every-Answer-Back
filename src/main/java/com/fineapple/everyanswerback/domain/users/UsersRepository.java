02df98e819b7a06326a40e7db8c275ba1ec239fc
kohj1018 <kohj1018@hanyang.ac.kr>
2023-01-23 05:59:55 +0900

    [fix] user api isDelete가 false일 때 조회 가능하도록 수정

--- src/main/java/com/fineapple/everyanswerback/domain/users/UsersRepository.java
+++ src/main/java/com/fineapple/everyanswerback/domain/users/UsersRepository.java
@@ -7,9 +7,9 @@ import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
-    @Query(value = "SELECT u FROM Users u WHERE u.oauthId = ?1")
+    @Query(value = "SELECT u FROM Users u WHERE u.oauthId = ?1 AND u.isDelete = false")
     Optional<Users> findByOauthId(String oauthId);
 
-    @Query(value = "SELECT u FROM Users u WHERE u.nickname = ?1")
+    @Query(value = "SELECT u FROM Users u WHERE u.nickname = ?1 AND u.isDelete = false")
     Optional<Users> findByNickname(String nickname);
 }
