dec1704a5caa299d908540e51a5bc4d52762df07
kohj1018 <kohj1018@hanyang.ac.kr>
2023-01-25 08:54:13 +0900

    webconfig, swaggerconfig 수정

--- src/main/java/com/fineapple/everyanswerback/SwaggerConfig.java
+++ src/main/java/com/fineapple/everyanswerback/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.fineapple.everyanswerback;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(servers = {@Server(url = "https://everyanswer.xyz", description = "Default Server URL")})
 
 @Configuration
 public class SwaggerConfig {
@@ -23,6 +27,6 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("EveryAnswer API")
                         .description("EveryAnswer 프로젝트 API 명세서입니다.")
-                        .version("v0.0.6"));
+                        .version("v0.1.0"));
     }
 }
